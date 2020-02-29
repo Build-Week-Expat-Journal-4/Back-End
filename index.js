5eb13eb933cb917691d718d3bd1b2aa35ada42b2
unknown <viola4lfe@gmail.com>
2020-02-28 22:19:47 -0500

    installed dependencies: bcrypt, cors, cross-env, dotenv, express, helmet, jsonwebtoken, knex, nodemon, pg, sqlite3, supertest, jest; configured scripts; setup server and index file

---
+++ index.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+const server = require("./api/server.js");
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => console.log(`\n*** Server running on port: ${PORT} ***\n`))
