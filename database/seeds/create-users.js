45eb903491bc7c91a2e674327d4c1ced24d40f77
unknown <viola4lfe@gmail.com>
2020-03-03 17:19:12 -0500

    added user seeds

---
+++ database/seeds/create-users.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        {
+          first_name: "John",
+          last_name: "Doe",
+          email: "tester@test.com",
+          username: "test1",
+          password: "test1"
+        },
+        {
+          first_name: "Sally",
+          last_name: "Doe",
+          email: "tester@test.com",
+          username: "test2",
+          password: "test2"
+        },
+        {
+          first_name: "Jim",
+          last_name: "Doe",
+          email: "tester@test.com",
+          username: "test3",
+          password: "test3"
+        }
+      ]);
+    });
+};
